b702b7ea82429892fd4aa49c8b1cba5d5b9a1e3e
daniel Kabagambe <danielkabagambe@gmail.com>
2024-11-16 18:24:29 +0300

    const settings = {   infinite: true,   speed: 500,   slidesToShow: 3,   slidesToScroll: 1,   autoplay: true,   autoplaySpeed: 3000,   responsive: [     {       breakpoint: 767,       settings: {         slidesToShow: 2,         slidesToScroll: 2,       },     },     {       breakpoint: 480,       settings: {         slidesToShow: 1,         slidesToScroll: 1,       },     },   ],   nextArrow: <div />,   prevArrow: <div />, };

--- client/src/pages/SafariSection.js
+++ client/src/pages/SafariSection.js
@@ -16,17 +16,25 @@ const SafariSection = () => {
     speed: 500,
     slidesToShow: 3,
     slidesToScroll: 1,
-    autoplay: true, // Enable autoplay
+    autoplay: true,
     autoplaySpeed: 3000,
     responsive: [
       {
-        breakpoint: 768,
+        breakpoint: 767,
         settings: {
           slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
         },
       },
     ],
-    nextArrow: <div />, // Hide default arrows
+    nextArrow: <div />,
     prevArrow: <div />,
   };
 
